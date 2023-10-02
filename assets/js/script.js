48059502583e9742093d696d9d09852b3163b101
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-10-02 01:31:21 -0500

    homework 6 changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,12 +39,12 @@ $(document).ready(function () {
     }
     function buildURLFromInputs(city) {
         if (city) {
-            return 'https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid={API key}';
+            return 'https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid={APIkey}';
         }
     }
 
     function buildURLFromID(id) {
-        return 'https://api.openweathermap.org/data/2.5/forecast?id=${id}&appid={API key}';
+        return 'https://api.openweathermap.org/data/2.5/forecast?id=${id}&appid={APIkey}';
     }
 
     function displayCities(pastCities) {
@@ -60,5 +60,77 @@ $(document).ready(function () {
 
         });
     }
+    function setUVIndexColor(uvi) {
+        if (uvi < 3) {
+            return 'green';
+        } else if (uvi >= 3 && uvi < 6) {
+            return 'yellow';
+        } else if (uvi >= 6 && uvi < 8) {
+            return 'orange';
+        } else if (uvi >= 8 && uvi < 11) {
+            return 'red';
+        } else return 'purple';
+    }
+
+    function searchWeather(queryURL) {
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function (response) {
+
+            let city = response.name;
+            let id = response.id;
+
+            if (pastCities[0]) {
+                pastCities = $.grep(pastCities, function (storedCity) {
+                    return id !== storedCity.id;
+                })
+            }
+            pastCities.unshift({ city, id});
+            storedCities();
+            displayCities(pastCities);
+
+            cityEl.text(response.name);
+            let formattedDate = moment.unix(response.dt).format('L');
+            dateEl.text(formattedDate);
+            let weatherIcon = response.weather[0].icon;
+            weatherIcon.attr('src', 'http://openweathermap.org/img/wn/${weatherIcon}.png').attr('alt', response.weather[0].description);
+            temperatureEl.html(((response.main.temp - 273.15) * 1.8 + 32).toFixed(1));
+            humidityEl.text(response.main.humidity);
+            windEl.text((response.wind.speed * 2.237).toFixed(1));
+
+            let lat = response.coord.lat;
+            let lon = response.coord.lon;
+            let queryURLAll = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={apikey}';
+            $.ajax({
+                url: queryURLAll,
+                method: 'GET'
+            }).then(function (response) {
+                let uvIndex = response.current.uvi;
+                let uvColor = setUVIndexColor(uvIndex);
+                uvIndexEl.text(response.current.uvi);
+                uvIndexEl.attr('style', 'background-colorL: $(uvColor); color: $(uvColor === "yellow" ? "black" : "white"}');
+                let fiveDay = response.daily;
+
+                for (let i = 0; i <=5; i++) {
+                    let currentDay = fiveDay[i];
+                    $('div.day-${i} .card-titel').text(moment.unix(currentDay.dt).format('L'));
+                    $('div.day-${i} .fiveDay-img').attr(
+                        'src',
+                        'https://http://openweathermap.org/img/wn/${currDay.weather[0].icon}.png'
+                    ).attr('alt', currDay.weather[0].description);
+                    $('div.day-${i} .fiveDay-temp').text(((currDay.temp.day - 273.15) * 1.8 + 32).toFixed(1));
+                    $('div.day-${1} .fiveDay-humid').text(currDay.humidity)
+                };
+            });
+        });
+    }
+    function displayLastSearchedCity() {
+        if (pastCities[0]) {
+            let queryURL
+        }
+    }
+
 })
 
